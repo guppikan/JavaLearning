2dbb95c436ce0ff803be46208dfc444559939a97
guppikan83 <guppikan83@yahoo.com>
2022-08-31 13:28:32 +0000

    Adding sum of two integers java file to repo

---
+++ SumOfIntegers.java
@@ -0,0 +1,16 @@
+// Java program to add two integers 
+ 
+class SumOfIntegers{
+
+	public static void main (String[] args){
+
+		int a =10;
+		int b =20;
+		int sum;
+
+		sum =a+b;
+		System.out.println("The sum of two integers is: " +sum);
+	}
+
+}
+
